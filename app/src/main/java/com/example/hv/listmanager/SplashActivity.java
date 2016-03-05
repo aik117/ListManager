0380b9949d4198bfafd4af30164382f88e2429a0
aamish <aamish@gmail.com>
2016-03-05 11:33:37 +0500

    first commit of listmanager

---
+++ app/src/main/java/com/example/hv/listmanager/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.hv.listmanager;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                finish();
+                startActivity(mainIntent);
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
